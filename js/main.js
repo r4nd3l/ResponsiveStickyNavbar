7f095905276b4580bbd8b002ed143f9e2b6ccc5c
matemolnar88 <matemolnar88@gmail.com>
2019-02-14 12:27:43 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Default DevCorner JavaScript Setting
+
+// Menu toggle-button
+$(document).ready(function(){
+  $(".menu-icon").on("click", function(){
+    $("nav ul").toggleClass("showing");
+  })
+})
+
+// Scrolling Effect
+$(window).on("scroll", function(){
+  if($(window).scrollTop()){
+    $('nav').addClass('green');
+  }else{
+    $('nav').removeClass('green');
+  }
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+Html, Css and jQuery based Responsive Sticky Navbar website - exercise
+---
+
+![ResponsiveStickyNavbar](https://github.com/r4nd3l/ResponsiveStickyNavbar/blob/master/img/sample.gif)
